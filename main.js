b49b1bd18517ec00f644e21e7e83f486a16059e9
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-09-17 16:14:11 +0100

    Initialised HTML/CSS/JS by copying from velut-json-generator

---
+++ main.js
@@ -0,0 +1,268 @@
+const buttonClearInputs = document.getElementById("clear-inputs");
+const buttonLoadSampleData = document.getElementById("load-sample-data");
+const textareaInput = document.getElementById("textarea-input");
+const textByGenerateJson = document.getElementById("text-by-generate-json");
+const buttonGenerateJson = document.getElementById("generate-json");
+const textareaOutput = document.getElementById("textarea-output");
+const textByCopyToClipboard = document.getElementById("text-by-copy-to-clipboard");
+const buttonCopyToClipboard = document.getElementById("copy-to-clipboard");
+const buttonDownload = document.getElementById("download");
+
+textareaOutput.value = "";
+
+
+//// Schema. Commented-out properties will not be used.
+
+const wordsSchema = {
+    Ord: "int",
+    Word: "string",
+    // Lemmata: "string",
+    Length: "int",
+    AllConsonants: "string",
+    Uncompounded: "string",
+    Phonetic: "string",
+    Scansion: "string",
+    ScansionWithElision: "string",
+    IsFitForDactyl: "int",
+    AllVowels: "string",
+    SyllableCount: "int",
+    Stress: "int",
+    UltimaRhyme: "string",
+    RhymeVowels: "string",
+    PerfectRhyme: "string",
+    RhymeConsonants: "string",
+    // Ultima: "string",
+    RhymeVowelsAndUltimaCoda: "string",
+    EcclesPhonetic: "string",
+    EcclesVowels: "string",
+    EcclesRhymeVowels: "string",
+    EcclesRhymeVowelsAndUltimaCoda: "string",
+    EcclesPerfectRhyme: "string",
+    EcclesSort: "string",
+    LemmaCount: "int",
+    // Lemma1: "string",
+    // Lemma2: "string",
+    // Lemma3: "string",
+    // Lemma4: "string",
+    // Lemma5: "string",
+    LemmaArray: "array",
+    IsLemma: "int",
+    IsNonLemma: "int",
+    // DuplicateWords: "string",
+    // NewLemmata: "string",
+    NoMacra: "string",
+    NoMacraLowerCase: "string",
+    AlphOrderNoMacra: "string",
+    Sort: "string",
+    // RepeatWord: "string",
+};
+
+
+//// Sample data the user can load if they don’t have my Excel file:
+
+const sampleData =
+`vocābulōrum	vocābulum
+excellentium	excellēns excellō
+Latīnōrum	Latīnus[prn] Latīnus[adj]
+ūtilēs	ūtilis
+tabulae	tabula
+`
+
+const expectedOutputFromSampleData =
+`Ord	Word	Lemmata	Length	AllConsonants	Uncompounded	Phonetic	Scansion	AllVowels	SyllableCount	Stress	UltimaRhyme	RhymeVowels	PerfectRhyme	RhymeConsonants	Ultima	RhymeVowelsAndUltimaCoda	EcclesPhonetic	EcclesVowels	EcclesRhymeVowels	EcclesRhymeVowelsAndUltimaCoda	EcclesPerfectRhyme	EcclesSort	LemmaCount	Lemma1	Lemma2	Lemma3	Lemma4	Lemma5	ScansionWithElision	IsFitForDactyl	LemmaArray	IsLemma	IsNonLemma	DuplicateWords	NewLemmata	NoMacra	NoMacraLowerCase	AlphOrderNoMacra	Sort
+89780	vocābulōrum	vocābulum	11	vcblrm	vocābulōrum	vocābulōrũ	⏑–⏑––	oāuōũ	5	2	ũ	ōũ	ōrũ	aram	'2 ũ	ōũ	vocabulorum	oauou	ou	oum	orum	ou-aram-uao-labacav-vocazzzzbulozzzzrum/	1	vocābulum					⏑–⏑–	0	["vocābulum"]	0	1			vocabulorum	vocabulorum	abclmooruuv	ozzzzuzzzzzz-ara-uazzzzo-labazzzzcav-vocābulōrum/
+89781	excellentium	excellēns excellō	12	xcllntm	excellentium	ecscellentiũ	–––⏑–	eeeiũ	5	3	ũ	eiũ	entiũ	antaam	ũ	eiũ	ecscellentium	eeeiu	eiu	eium	entium	eiu-antaam-ee-llacsca-excellentium/	2	excellēns	excellō				–––⏑	1	["excellēns","excellō"]	0	1			excellentium	excellentium	ceeeillmntux	eiuzzzzzz-antaa-ee-llacsca-excellentium/
+89782	Latīnōrum	Latīnus[prn] Latīnus[adj]	9	ltnrm	Latīnōrum	latīnōrũ	⏑–––	aīōũ	4	2	ũ	ōũ	ōrũ	aram	'2 ũ	ōũ	latinorum	aiou	ou	oum	orum	ou-aram-ia-natal-latizzzznozzzzrum	2	Latīnus[prn]	Latīnus[adj]				⏑––	1	["Latīnus[prn]","Latīnus[adj]"]	0	1			Latinorum	latinorum	ailmnortu	ozzzzuzzzzzz-ara-izzzza-nizzzztal-latīnōrum
+`
+
+
+//// Functions used in `generateJson`:
+
+const getSchemaFromHeaderRow = (headerRow) => {
+    switch (headerRow[1]) {
+        case "Word":
+            tableName = "words";
+            return wordsSchema;
+        case "Lemma":
+            tableName = "lemmata";
+            return lemmataSchema;
+        default:
+            tableName = "custom";
+            return generateSchemaFromCustomHeaderRow(headerRow);
+    }
+}
+
+const generateSchemaFromCustomHeaderRow = (headerRow) => {
+    let newSchema = {};
+    for (let i = 0; i < headerRow.length; i++) {
+        newSchema[headerRow[i]] = "string";
+    }
+    return newSchema;
+}
+
+const getEmptyTextRepresentation = (headerRow) => {
+    switch (headerRow[1]) {
+        case "Word":
+            return "0";
+        default:
+            return "null";
+    }
+}
+
+const getLastKey = (schema) => {
+    const keys = Object.keys(schema);
+    return keys[keys.length - 1];
+}
+
+const clearTextMessages = () => {
+    textByGenerateJson.textContent = "";
+    textByCopyToClipboard.textContent = "";
+}
+
+const clearInputs = () => {
+    textareaInput.value = "";
+    textareaOutput.value = "";
+    clearTextMessages();
+}
+
+const warnOfEmptyInput = () => {
+    clearTextMessages();
+    textByGenerateJson.textContent = "Nothing to generate Json from!";
+}
+
+const warnOfEmptyOutput = () => {
+    clearTextMessages();
+    textByCopyToClipboard.textContent = "Nothing to copy or download!";
+}
+
+let tableName = "custom"; //// "custom", "lemmata", "words".
+
+//// `outputArray` gets modified by `generateJson` and displayed in the second text-area by `displayOutput`.
+
+let outputArray = [];
+
+const output = (line) => {
+    outputArray.push(line);
+}
+
+
+//// Functions called by buttons:
+
+const generateJson = () => {
+    clearTextMessages();
+    textByGenerateJson.textContent = "Generating Json, please wait...";
+    outputArray.length = 0; // Clear the output in case there’s anything from previous runs.
+    const allInputRows = textareaInput.value.split("\n");
+    const headerRow = allInputRows[0].split("\t");
+    const schema = getSchemaFromHeaderRow(headerRow);
+    const emptyTextReplacement = getEmptyTextRepresentation(headerRow); // Empty string fields are represented differently in “words” than in “lemmata”.
+    const lastKeyInSchema = getLastKey(schema); // This is used to prevent trailing commas.
+    const countColumnsInInput = headerRow.length;
+    const countRows = allInputRows.length;
+
+    //// For each line of values in the input...
+    //// (We skip i==0 because that’s the header row.)
+    for (let i = 1; i < countRows; i++) {
+        //// Skip empty lines.
+        if (allInputRows[i] == "") { continue; }
+
+        const rowOfValues = allInputRows[i].split("\t");
+        output("{");
+
+        //// Create an object that maps each key in `headerRow` to the value in the current row.
+        let valuesAsObject = {};
+        for (let j = 0; j < countColumnsInInput; j++) {
+            const currentKey = headerRow[j];
+            const currentValue = rowOfValues[j];
+            valuesAsObject[currentKey] = currentValue;
+        }
+
+        //// Fields will be added to the output in the order they appear in the schema.
+        for (let currentKey in schema) {
+            const currentValue = valuesAsObject[currentKey];
+            //// Decide whether there should be a comma after the key–value pair.
+            const lineTerminator = currentKey == lastKeyInSchema ? "" : ",";
+            //// Use the types defined in the schema to determine the format.
+            //// Strings need to be quoted, but other values do not.
+            switch (schema[currentKey]) {
+                case "int":
+                    output(`"${currentKey}": ${currentValue ? currentValue : "null"}${lineTerminator}`);
+                    break;
+                case "string":
+                    output(`"${currentKey}": ${currentValue ? `"${currentValue}"` : emptyTextReplacement}${lineTerminator}`);
+                    break;
+                case "array":
+                    //// My value from Excel is always valid Json for an array of strings.
+                    output(`"${currentKey}": ${currentValue}${lineTerminator}`);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        output("}")
+    }
+    displayOutput();
+    textByGenerateJson.textContent = "Json generated!";
+}
+
+const displayOutput = () => {
+    textareaOutput.value = outputArray.join("\n");
+}
+
+const copyToClipboard = () => {
+    clearTextMessages();
+    textByCopyToClipboard.textContent = "Copying to clipboard...";
+    textareaOutput.select();
+    document.execCommand("copy");
+    textByCopyToClipboard.textContent = "Copied!";
+}
+
+const download = () => {    
+    let a = document.createElement('a');
+    a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textareaOutput.value.replace(/\n/g, "\r\n")));;
+    a.setAttribute('download', tableName + "_mongo.json");
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    clearTextMessages();
+}
+
+
+//// Event listeners.
+
+buttonClearInputs.addEventListener("click", ()=>{
+    clearInputs();
+});
+
+buttonLoadSampleData.addEventListener("click", ()=>{
+    textareaInput.value = sampleData;
+    clearTextMessages();
+});
+
+buttonGenerateJson.addEventListener("click", ()=>{
+    if (textareaInput.value === "") {
+        warnOfEmptyInput();
+    }
+    else {
+        generateJson();
+    }
+});
+
+buttonCopyToClipboard.addEventListener("click", ()=>{
+    if (textareaOutput.value === "") {
+        warnOfEmptyOutput();
+    }
+    else {
+        copyToClipboard();
+    }
+});
+
+buttonDownload.addEventListener("click", ()=>{
+    if (textareaOutput.value === "") {
+        warnOfEmptyOutput();
+    }
+    else {    
+        download();
+    }
+});
