3e76e5f5a923f2859a1b358c7484b1cc82d0dce8
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-09-17 16:48:25 +0100

    Object of empty JS functions

---
+++ velut-wordsform-functions.js
@@ -0,0 +1,168 @@
+const f = {
+	Ord:
+		(word, lemmata) => {
+			return '';
+		},
+	Word:
+		(word, lemmata) => {
+			return '';
+		},
+	Lemmata:
+		(word, lemmata) => {
+			return '';
+		},
+	Length:
+		(word, lemmata) => {
+			return '';
+		},
+	AllConsonants:
+		(word, lemmata) => {
+			return '';
+		},
+	Uncompounded:
+		(word, lemmata) => {
+			return '';
+		},
+	Phonetic:
+		(word, lemmata) => {
+			return '';
+		},
+	Scansion:
+		(word, lemmata) => {
+			return '';
+		},
+	AllVowels:
+		(word, lemmata) => {
+			return '';
+		},
+	SyllableCount:
+		(word, lemmata) => {
+			return '';
+		},
+	Stress:
+		(word, lemmata) => {
+			return '';
+		},
+	UltimaRhyme:
+		(word, lemmata) => {
+			return '';
+		},
+	RhymeVowels:
+		(word, lemmata) => {
+			return '';
+		},
+	PerfectRhyme:
+		(word, lemmata) => {
+			return '';
+		},
+	RhymeConsonants:
+		(word, lemmata) => {
+			return '';
+		},
+	Ultima:
+		(word, lemmata) => {
+			return '';
+		},
+	RhymeVowelsAndUltimaCoda:
+		(word, lemmata) => {
+			return '';
+		},
+	EcclesPhonetic:
+		(word, lemmata) => {
+			return '';
+		},
+	EcclesVowels:
+		(word, lemmata) => {
+			return '';
+		},
+	EcclesRhymeVowels:
+		(word, lemmata) => {
+			return '';
+		},
+	EcclesRhymeVowelsAndUltimaCoda:
+		(word, lemmata) => {
+			return '';
+		},
+	EcclesPerfectRhyme:
+		(word, lemmata) => {
+			return '';
+		},
+	EcclesSort:
+		(word, lemmata) => {
+			return '';
+		},
+	LemmaCount:
+		(word, lemmata) => {
+			return '';
+		},
+	Lemma1:
+		(word, lemmata) => {
+			return '';
+		},
+	Lemma2:
+		(word, lemmata) => {
+			return '';
+		},
+	Lemma3:
+		(word, lemmata) => {
+			return '';
+		},
+	Lemma4:
+		(word, lemmata) => {
+			return '';
+		},
+	Lemma5:
+		(word, lemmata) => {
+			return '';
+		},
+	ScansionWithElision:
+		(word, lemmata) => {
+			return '';
+		},
+	IsFitForDactylic:
+		(word, lemmata) => {
+			return '';
+		},
+	LemmaArray:
+		(word, lemmata) => {
+			return '';
+		},
+	IsLemma:
+		(word, lemmata) => {
+			return '';
+		},
+	IsNonLemma:
+		(word, lemmata) => {
+			return '';
+		},
+	DuplicateWords:
+		(word, lemmata) => {
+			return '';
+		},
+	NewLemmata:
+		(word, lemmata) => {
+			return '';
+		},
+	NoMacra:
+		(word, lemmata) => {
+			return '';
+		},
+	NoMacraLowerCase:
+		(word, lemmata) => {
+			return '';
+		},
+	AlphOrderNoMacra:
+		(word, lemmata) => {
+			return '';
+		},
+	Sort:
+		(word, lemmata) => {
+			return '';
+		},
+	RepeatWord:
+		(word, lemmata) => {
+			return '';
+		},
+}
+
+const wordsformFunctions = f;
